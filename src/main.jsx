2bdf4e740dcfcf14ddb0da7a4f7d2676949251bf
Nahuelsand <rod.arena7@gmail.com>
2026-02-23 00:54:02 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppSelector from './AppSelector'; // Cambiado de './App' a './AppSelector'
-import './index.css'; // Asegúrate de tener este archivo o quita esta línea
+import AppSelector from './AppSelector';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
